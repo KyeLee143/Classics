e6d27e10d24899eee71eb830b392924f81c91fd7
KyeLee143 <54326810+KyeLee143@users.noreply.github.com>
2019-11-15 09:50:09 -0800

    Create webapp.py

---
+++ webapp.py
@@ -0,0 +1,23 @@
+from flask import Flask, url_for, render_template, request
+
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+
+@app.route("/")
+def render_main():
+    return render_template('home.html')
+    
+    
+    @app.route("/jsonInfo")
+def render_page1():
+    return render_template('InfoAvailable.html')
+    
+@app.route("/EvenMoreInfo")
+def render_page2():
+    return render_template('StillCurious.html')
+
+@app.route("/links")
+def render_page3():
+    return render_template('GreatSources.html')
+    
+    if __name__=="__main__":
+    app.run(debug=False, port=54321)
